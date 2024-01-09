6536805582a43c15ff56817b1e73c147e8fd883d
RBCodeJourney <refatullahbiswas@gmail.com>
2024-01-10 01:09:30 +0530

    second commit

---
+++ js/prompt.js
@@ -0,0 +1,23 @@
+// alert('Hello! look at me!!');
+
+const alertUser = () => {
+    alert('Be attentive and serious to invest time for learning!')
+}
+
+const seriousStudent = () => {
+    const serious = confirm('Are you spending 6-8hrs every single day to learn web dev? ');
+    console.log(serious);
+
+    if (serious === true) {
+        alert('Well done! keep it up.')
+    }
+    else {
+        console.log('Valo hoye jao !!! kopale dukkho ase!')
+    }
+};
+
+
+const getName = () => {
+   const name = prompt('What is your Name?')
+   console.log(name);
+}
